3752bcf86c700320fa7cdbd65fb97e8091cd586b
Sophia Lichterfeld <sophialichterfeld@Sophias-MacBook-Air.local>
2020-10-31 12:55:25 -0400

    distance sensor testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistSens.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.*;
+@Autonomous
+
+public class DistSens extends LinearOpMode {
+    Rev2mDistanceSensor DistSenTop;
+    Rev2mDistanceSensor DistSenBottom;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        DistSenTop = hardwareMap.get(Rev2mDistanceSensor.class,"DistSenTop");
+        DistSenBottom = hardwareMap.get(Rev2mDistanceSensor.class, "DistSenBottom");
+
+        telemetry.addData("status", "initialized");
+        telemetry.update();
+
+        waitForStart();
+        while(opModeIsActive()){
+            telemetry.addData("Top Sensor Value", DistSenTop.getDistance(DistanceUnit.CM));
+            telemetry.addData("Bottom Sensor Value", DistSenBottom.getDistance(DistanceUnit.CM));
+            telemetry.update();
+        }
+    }
+}
