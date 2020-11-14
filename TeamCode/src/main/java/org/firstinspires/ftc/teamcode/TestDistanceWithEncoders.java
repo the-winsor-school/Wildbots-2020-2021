f006a3e8195c53f0c33afd1323638764f1cc3c95
Abigail Bohl <abohl@winsor.edu>
2020-11-14 16:49:33 -0500

    more localization and a new teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDistanceWithEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDistanceWithEncoders.java
@@ -16,7 +16,7 @@ public class TestDistanceWithEncoders extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        //setting up stuff :O
         drivingLibrary = new DrivingLibrary(this);
         drivingLibrary.setSpeed(1);
         drivingMode = 0;
@@ -24,12 +24,14 @@ public class TestDistanceWithEncoders extends LinearOpMode {
         boolean ranOnce = false;
         telemetry.addData("status", "initilized");
         telemetry.update();
+
         drivingLibrary.resetEncoderValues();
         drivingLibrary.setEncoders(12);
         drivingLibrary.setRunMode(true);
+
         int[] values = drivingLibrary.getEncoderValues();
         drivingLibrary.setRecordEncoderTable();
-
+        //variables stand for veria(ble) fun
         double deltaX;
         double deltaY;
         double xPos=0;
@@ -40,12 +42,13 @@ public class TestDistanceWithEncoders extends LinearOpMode {
             drivingLibrary.bevelDrive(0, -.75f, 0);
 
             values=drivingLibrary.getEncoderValues();
-
+            //get the deltas which you get in the library
             deltaX= drivingLibrary.getDeltaX();
             xPos+=deltaX;
             deltaY=drivingLibrary.getDeltaY();
             yPos+=deltaY;
             values=drivingLibrary.getEncoderValues();
+            //add telemetry data
             telemetry.addData("xpos:", xPos);
             telemetry.addData("yPos", yPos);
             telemetry.addData("encoder values:", Arrays.toString(values));
