6f5117af7af23673321a411c1a2215590c47a3a0
lizakuntz <elizabeth.kuntz@winsor.edu>
2020-12-06 10:41:42 -0500

    some random afterthought comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerShot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerShot.java
@@ -9,10 +9,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 //Names the Auton -> "name =" appears on the phone
 @Autonomous(name = "JustThePowerShot")
 public class PowerShot extends LinearOpMode {
-
-
+    public static final int ONE_SECOND = 1000;
     private DrivingLibrary drivingLibrary;
-    //target dist is the distance the robot should be from the right wall
+    //target 1 dist is the distance the robot should be from the right wall
     //when it is lined up with power shot 1
     //NOTE: measure what the actual distances are and don't GUESS DUMMY
     private static final double TARGET_1_DISTANCE = 36.0;
@@ -32,8 +31,7 @@ public class PowerShot extends LinearOpMode {
         // launch line
 
         waitForStart();
-        // when the robot is at the wrong distance, move until you reach or pass it
-        // get distance from right wall using dist sens
+        // all of this is just calling methods (see that we have not fully fleshed out takeShot)
         moveToDistance(TARGET_1_DISTANCE);
         takeShot();
         moveToDistance(TARGET_2_DISTANCE);
@@ -44,12 +42,14 @@ public class PowerShot extends LinearOpMode {
 
     // when we know how, put in the code for launching
     private void takeShot() throws InterruptedException {
-        Thread.sleep(1000);
+        //pause for 1 second
+        Thread.sleep(ONE_SECOND);
     }
 
+    // robot moves until it reaches one of the constant distance away from the right wall
     private void moveToDistance(double targetDistance) {
         double distance = drivingLibrary.distSenBottom.getDistance(DistanceUnit.INCH);
-
+        // when the actual distance is greater than target dist, robot keeps moving
         while (distance >= targetDistance) {
             //sets the direction robot will move in (Is 1 too fast?)
             drivingLibrary.bevelDrive(1, 0, 0);
