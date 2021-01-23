b20aa6d62f228fa42fcfbe59325fe24e93d21bf5
lizakuntz <elizabeth.kuntz@winsor.edu>
2021-01-23 12:03:24 -0500

    added driving to parkLine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonPark.java
@@ -10,15 +10,15 @@ public class AutonPark extends LinearOpMode {
 
     private DrivingLibrary drivingLibrary;
     //variables:
-    private static final float crashIntoWall = .50f; //figure out actual time but this is the time it takes to get to the left wall
-    private static final long parkLine = 1000; //Time to get to line
+    private static final long crashIntoWall = 500; //figure out actual time but this is the time it takes to get to the left wall
+    private static final long parkLine = 4000; //Time to get to line
 
     //initializing
     @Override
     public void runOpMode() throws InterruptedException {
         drivingLibrary = new DrivingLibrary(this);
         drivingLibrary.setSpeed(1.0);
-        telemetry.addData("status", "BAMMMMM initialized");
+        telemetry.addData("status", "BAAAAAAAH initialized");
         telemetry.update();
 
         //ensures that the code will only run once
@@ -29,12 +29,14 @@ public class AutonPark extends LinearOpMode {
         //drive for (crashIntoWall) speed
         if (opModeIsActive()) {
             if (!ranOnce) {
-                drivingLibrary.drive(0, crashIntoWall, 0);
-                sleep(500);
+                drivingLibrary.drive(0, .5f, 0);
+                sleep(crashIntoWall);
+                drivingLibrary.brakeStop();
+                drivingLibrary.drive(.5f, 0, 0);
+                sleep(parkLine);
                 drivingLibrary.brakeStop();
                 ranOnce = true;
             }
-
         }
     }
 }
