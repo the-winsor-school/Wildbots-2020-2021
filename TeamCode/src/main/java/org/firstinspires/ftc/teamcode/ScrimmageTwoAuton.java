2d936e5972255ebcaeaa2f3c5bb3a9cb777b48ac
lizakuntz <elizabeth.kuntz@winsor.edu>
2021-02-13 12:59:37 -0500

    imu??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimmageTwoAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimmageTwoAuton.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.libraries.AutonLibrary;
 import org.firstinspires.ftc.libraries.DrivingLibrary;
@@ -17,6 +18,8 @@ public class ScrimmageTwoAuton extends LinearOpMode {
 
     Rev2mDistanceSensor distTop;
     Rev2mDistanceSensor distBot;
+    Servo leftWobble;
+    Servo rightWobble;
 
     //initializing
     @Override
@@ -26,8 +29,11 @@ public class ScrimmageTwoAuton extends LinearOpMode {
         telemetry.addData("status", "initialized");
         telemetry.update();
 
-        distTop = hardwareMap.get(Rev2mDistanceSensor.class, "topSensor");
-        distBot = hardwareMap.get(Rev2mDistanceSensor.class, "bottomSensor");
+        distTop = hardwareMap.get(Rev2mDistanceSensor.class, "distTop");
+        distBot = hardwareMap.get(Rev2mDistanceSensor.class, "distBot");
+        leftWobble = hardwareMap.get(Servo.class, "leftWobble");
+        rightWobble = hardwareMap.get(Servo.class, "rightWobble");
+
 
         AutonLibrary autonLibrary = null;
         //ensures that the code will only run once
@@ -35,57 +41,65 @@ public class ScrimmageTwoAuton extends LinearOpMode {
 
         waitForStart();
 
-        if (opModeIsActive()) {
-            if (!ranOnce) {
-                //moves left
-                drivingLibrary.bevelDrive(-.5f, -.5f, 0);
-                sleep(2000);
-                //moves forward to park line
-                drivingLibrary.bevelDrive(0, -.5f, 0);
-                sleep(8500);
-                drivingLibrary.brakeStop();
+        if (opModeIsActive()) if (!ranOnce) {
+            leftWobble.setPosition(-1);
+            rightWobble.setPosition(1);
+            //moves left
+            drivingLibrary.bevelDrive(-.5f, -.5f, 0);
+            sleep(2000);
+            //moves forward to park line
+            drivingLibrary.bevelDrive(0, -.5f, 0);
+            sleep(8500);
+            drivingLibrary.brakeStop();
 
-                numRings = autonLibrary.getStackHeight(distTop, distBot);
+            numRings = autonLibrary.getStackHeight(distTop, distBot);
 
-                switch (numRings) {
-                    case 0:
-                        //move right
-                        drivingLibrary.bevelDrive(.5f, 0, 0);
-                        sleep(2000);
-                        drivingLibrary.brakeStop();
-                        //move backwards
-                        drivingLibrary.bevelDrive(0, .5f, 0);
-                        sleep(2000);
-                        drivingLibrary.brakeStop();
-                        break;
-                    case 1:
-                        // go right
-                        drivingLibrary.bevelDrive(.5f, 0, 0);
-                        sleep(2000);
-                        drivingLibrary.brakeStop();
-                        //go forwards
-                        drivingLibrary.bevelDrive(0, -.5f, 0);
-                        sleep(3000);
-                        drivingLibrary.brakeStop();
-                        break;
-                    case 4:
-                        drivingLibrary.bevelDrive(0, -.5f, 0);
-                        sleep(8500);
-                        drivingLibrary.brakeStop();
-                        //move right
-                        drivingLibrary.bevelDrive(.5f, 0, 0);
-                        sleep(2000);
-                        drivingLibrary.brakeStop();
-                        //move backwards
-                        drivingLibrary.bevelDrive(0, .5f, 0);
-                        sleep(8500);
-                        drivingLibrary.brakeStop();
-                        break;
+            switch (numRings) {
+                case 0:
+                    //move right
+                    drivingLibrary.bevelDrive(.5f, 0, 0);
+                    sleep(2000);
+                    drivingLibrary.brakeStop();
+                    //move backwards
+                    drivingLibrary.bevelDrive(0, .5f, 0);
+                    sleep(2000);
+                    drivingLibrary.brakeStop();
+                    break;
+                case 1:
+                    // go right
+                    drivingLibrary.bevelDrive(.5f, 0, 0);
+                    sleep(2000);
+                    drivingLibrary.brakeStop();
+                    //go forwards
+                    drivingLibrary.bevelDrive(0, -.5f, 0);
+                    sleep(3000);
+                    drivingLibrary.brakeStop();
+                    break;
+                case 4:
+                    drivingLibrary.bevelDrive(0, -.5f, 0);
+                    sleep(8500);
+                    drivingLibrary.brakeStop();
+                    //move right
+                    drivingLibrary.bevelDrive(.5f, 0, 0);
+                    sleep(2000);
+                    drivingLibrary.brakeStop();
+                    //move backwards
+                    drivingLibrary.bevelDrive(0, .5f, 0);
+                    sleep(8500);
+                    drivingLibrary.brakeStop();
+                    break;
+
+            }
+            while (Math.abs(drivingLibrary.getIMUAngle() - 0) > .05) {
+                if (drivingLibrary.getIMUAngle() > 0) { // check which direction we need to turn
+                    drivingLibrary.bevelDrive(0, 0, .1f);
+                } else {
+                    drivingLibrary.bevelDrive(0, 0, -.1f);
+
+                    ranOnce = true;
                 }
 
-                ranOnce = true;
             }
-
         }
     }
 }
